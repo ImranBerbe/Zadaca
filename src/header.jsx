ae52f78ce2bb88ce29988cc54dd201f6433a9517
ImranBerbe <imranberberovic@gmail.com>
2024-10-04 17:37:38 +0200

    Environment setup

---
+++ src/header.jsx
@@ -0,0 +1,13 @@
+function header () {
+    return (
+        <div class="header">
+            <nav class="Navbar">
+                <a href="#">Features</a>
+                <a href="#">About</a>
+                <a href="#">Newsletter</a>
+            </nav>
+        </div>
+    );
+}
+
+export default header
